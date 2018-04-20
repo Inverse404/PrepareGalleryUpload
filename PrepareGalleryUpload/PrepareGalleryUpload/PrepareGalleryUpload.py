721d250b468bcd53cde230dea5e0c0397e3a2b20
Niels Seidler <seidler@computer-science.de>
2018-04-20 20:05:12 +0200

    basic envira json gallery export, without config part

--- PrepareGalleryUpload/PrepareGalleryUpload/PrepareGalleryUpload.py
+++ PrepareGalleryUpload/PrepareGalleryUpload/PrepareGalleryUpload.py
@@ -6,6 +6,7 @@ import json
 #CONFIGURE your default directories here
 default_output_base_directory = "f:\\_temp\\rexx\\gallery upload"
 default_ffmpeg_path = "c:\\Tools\\ffmpeg\\ffmpeg.exe"
+default_uploaded_base_url = "http://rexxworld.com/ftp-uploads/galleries/"
 
 #retrieve command line argument to identify from qhich source directory to read for source video files
 arguments_parser = argparse.ArgumentParser(description='Prepare web upload gallery files from master videos.')
@@ -27,6 +28,8 @@ source_dir_path		= command_line_arguments.source_directory
 source_dir_name		= os.path.basename( source_dir_path )
 output_directory	= os.path.join( default_output_base_directory, source_dir_name )
 
+uploaded_gallery_url = default_uploaded_base_url + source_dir_name + "/"
+
 #ensure output directory is created if it does not yet exist
 if os.path.exists( output_directory ) == False:
 	os.makedirs( output_directory )
@@ -40,11 +43,15 @@ envira_gallery["gallery"] = {}
 envira_gallery["config"] = {}
 envira_gallery["in_gallery"] = []
 
+gallery_entry_id = 0
 for dir_entry in os.scandir( source_dir_path ):
 	entry_name, entry_extension = os.path.splitext( dir_entry.name )
 
 	if entry_extension.lower() == ".mp4":
 		#we have a video file, so let's go and call the usual external tools to process it
+		gallery_entry_id += 1
+		gallery_entry_id_str = str(gallery_entry_id)
+
 		output_file_name = entry_name.replace( "-UHD", "-HQ" )
 		#create a HQ web video file
 		subprocess.run( [	ffmpeg_path,
@@ -65,7 +72,33 @@ for dir_entry in os.scandir( source_dir_path ):
 							os.path.join(output_directory, output_file_name + ".jpg" )
 						] )
 
+
+		uploaded_video_url = uploaded_gallery_url + output_file_name + ".mp4"
+		uploaded_thumb_url = uploaded_gallery_url + output_file_name + ".jpg"
+
+		#create an entry in the envira gallery import json file
+		envira_gallery["gallery"][gallery_entry_id_str]={}
+		envira_gallery["gallery"][gallery_entry_id_str]["status"] = "active"
+		envira_gallery["gallery"][gallery_entry_id_str]["src"] = ""
+		envira_gallery["gallery"][gallery_entry_id_str]["title"] = output_file_name
+		envira_gallery["gallery"][gallery_entry_id_str]["link"] = uploaded_video_url
+		envira_gallery["gallery"][gallery_entry_id_str]["alt"] = ""
+		envira_gallery["gallery"][gallery_entry_id_str]["caption"] = ""
+		envira_gallery["gallery"][gallery_entry_id_str]["thumb"] = uploaded_thumb_url
+		envira_gallery["gallery"][gallery_entry_id_str]["tags"] = []
+		envira_gallery["gallery"][gallery_entry_id_str]["schedule_meta"] = 0
+		envira_gallery["gallery"][gallery_entry_id_str]["schedule_meta_start"] = ""
+		envira_gallery["gallery"][gallery_entry_id_str]["schedule_meta_end"] = ""
+		envira_gallery["gallery"][gallery_entry_id_str]["schedule_meta_ignore_date"] = 0
+		envira_gallery["gallery"][gallery_entry_id_str]["schedule_meta_ignore_year"] = 0
+		envira_gallery["gallery"][gallery_entry_id_str]["video_aspect_ratio"] = ""
+		envira_gallery["gallery"][gallery_entry_id_str]["video_width"] = ""
+		envira_gallery["gallery"][gallery_entry_id_str]["video_height"] = ""
+		envira_gallery["gallery"][gallery_entry_id_str]["video_in_gallery"] = ""
+
+		envira_gallery["in_gallery"].append( gallery_entry_id )
+
 envira_gallery_import_file_path = os.path.join(default_output_base_directory, source_dir_name + ".json")
-envira_gallery_import_file = open( envira_gallery_import_file_path, 'w' )
+envira_gallery_import_file = open( envira_gallery_import_file_path, 'w+' )
 json.dump( envira_gallery, envira_gallery_import_file )
 envira_gallery_import_file.close()
