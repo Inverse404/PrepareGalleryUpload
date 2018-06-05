929e7cbe4bf8741249b4416284ae2cb16044af2d
Niels Seidler <seidler@computer-science.de>
2018-06-05 07:47:33 +0200

    added script for bulk-processing archives files to web files
    the added script "prepare_all_sub_folders_4_web.py" accepts a directory as source
    it will scan for any subdirectory in the source directory and call the conversion routine that prepares the contents of a whole archive master video file directory for the web for each of the subdirectories contained in the given source directory (this means you supply  the master archive base directory to this script and will launch a convert operation for each of the directories found one after another)

---
+++ PrepareGalleryUpload/PrepareGalleryUpload/prepare_all_sub_folders_4_web.py
@@ -0,0 +1,18 @@
+import os
+
+from pgu_config import pgu_config
+import default_user_scripts
+
+
+config = pgu_config(None)
+
+#retrieve command line argument to identify from which source path to read from
+arguments_parser = argparse.ArgumentParser(description='Prepare many video gallery subdirectories for upload.')
+arguments_parser.add_argument('source')
+command_line_arguments = arguments_parser.parse_args()
+
+source = command_line_arguments.source
+
+for dir_entry in os.scandir( source ):
+	if dir_entry.is_dir() == True:
+		default_user_scripts.user_scripts.on_input_archive_directory( dir_entry.path(), config )
